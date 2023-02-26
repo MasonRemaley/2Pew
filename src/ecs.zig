4fc11d39a350e50585845619e96b3a8d4cbbb866
Mason Remaley <mason@anthropicstudios.com>
2023-02-26 01:54:06 -0800

    Starts allocating pages for component data
    Committed this WIP because I need to change direction. I need runtime
    access to the archetype for a page, but this design requires casting
    to it which means it needs to be known comptime.
    
    Instead, all pages should be the same time, and just have some buffer
    in which I can allocate the component data regardless of type.
    
    They'll also store the current archetype and dynamically do the right
    thing given the archetype.

--- src/ecs.zig
+++ src/ecs.zig
@@ -1,31 +1,106 @@
 const std = @import("std");
+const AutoArrayHashMapUnmanaged = std.AutoArrayHashMapUnmanaged;
 const builtin = @import("builtin");
 
 const max_entities: EntityIndex = 1000000;
 
 const EntityIndex = u32;
 
-const EntityGeneration = switch (builtin.mode) {
-    .Debug, .ReleaseSafe => u32,
-    .ReleaseSmall, .ReleaseFast => u0,
+const track_generation = switch (builtin.mode) {
+    .Debug, .ReleaseSafe => true,
+    .ReleaseSmall, .ReleaseFast => false,
 };
 
+const EntityGeneration = if (track_generation) u32 else u0;
+
+// XXX: pack and use all bits?
 pub const EntityHandle = struct {
     generation: EntityGeneration,
     index: EntityIndex,
 };
 
-pub fn Entities(comptime components: anytype) type {
-    return struct {
-        const Archetype: type = std.bit_set.IntegerBitSet(@typeInfo(@TypeOf(components)).Struct.fields.len);
+const EntitySlot = struct {
+    generation: EntityGeneration,
+};
 
-        entities: []EntityGeneration,
+pub fn Entities(comptime RegisteredComponents: type) type {
+    return struct {
+        const Archetype: type = std.bit_set.IntegerBitSet(@typeInfo(RegisteredComponents).Struct.fields.len);
+        const PageHeader = struct {
+            next: ?*anyopaque = null,
+        };
+
+        entities: []EntitySlot,
         free: []EntityIndex,
+        // XXX: better way to allocate this..? should we even be using the hashmap here?
+        pageLists: AutoArrayHashMapUnmanaged(Archetype, *anyopaque),
+
+        // XXX: maybe make helpers that convert between the various reprs..?
+        // XXX: naming of arg..?
+        // XXX: how does allocation/stack space/etc work during comptime?
+        fn Page(comptime EntityUnordered: type) type {
+            const Entity = Entity: {
+                comptime var fields: [@typeInfo(EntityUnordered).Struct.fields.len]std.builtin.Type.StructField = undefined;
+                comptime var i = 0;
+                inline for (@typeInfo(RegisteredComponents).Struct.fields) |registered| {
+                    inline for (@typeInfo(EntityUnordered).Struct.fields) |component| {
+                        if (std.mem.eql(u8, component.name, registered.name)) {
+                            fields[i] = std.builtin.Type.StructField{
+                                .name = registered.name,
+                                .type = registered.type,
+                                .default_value = null,
+                                .is_comptime = false,
+                                .alignment = @alignOf(registered.type),
+                            };
+                            i += 1;
+                            break;
+                        }
+                    }
+                }
+                if (i < fields.len) {
+                    // XXX: give better error, also prevent writing TOO MANY somehow etc or not possible?
+                    @compileError("not all fields found");
+                }
+                break :Entity @Type(std.builtin.Type{
+                    .Struct = std.builtin.Type.Struct{
+                        .layout = .Auto,
+                        .backing_integer = null,
+                        .fields = &fields,
+                        .decls = &[_]std.builtin.Type.Declaration{},
+                        .is_tuple = false,
+                    },
+                });
+            };
+
+            // Create the page type
+            const PageType = struct {
+                header: PageHeader,
+                entities: [std.mem.page_size - @sizeOf(PageHeader)]?Entity,
+
+                // XXX: maybe have an internal free list or such to accelerate this and early out when it's full etc
+                // XXX: make sure this cast is always safe at comptime?
+                fn createEntity(self: *@This()) ?u16 {
+                    for (self.entities, 0..) |entity, i| {
+                        if (entity) |_| {
+                            return @intCast(u16, i);
+                        }
+                    }
+                    return null;
+                }
+            };
+
+            // XXX: is there some better way to set the entities size so this always works..?
+            // // Make sure it's the right size, and then return it
+            // if (@sizeOf(PageType) != std.mem.page_size) {
+            //     @compileError("unreachable: Page struct is of wrong size");
+            // }
+            return PageType;
+        }
 
         fn init() !@This() {
             return .{
                 .entities = entities: {
-                    var entities = try std.heap.page_allocator.alloc(EntityGeneration, max_entities);
+                    var entities = try std.heap.page_allocator.alloc(EntitySlot, max_entities);
                     entities.len = 0;
                     break :entities entities;
                 },
@@ -34,15 +109,19 @@ pub fn Entities(comptime components: anytype) type {
                     free.len = 0;
                     break :free free;
                 },
+                .pageLists = .{},
             };
         }
 
         fn deinit(self: *@This()) void {
             std.heap.page_allocator.free(self.entities);
             std.heap.page_allocator.free(self.free);
+            self.pageLists.deinit(std.heap.page_allocator);
+            // XXX: actually free the pages...
         }
 
-        fn createEntityChecked(self: *@This()) ?EntityHandle {
+        fn createEntityChecked(self: *@This(), entity: anytype) ?EntityHandle {
+            // Find a free index for the entity
             const index = index: {
                 if (self.free.len > 0) {
                     // Pop an id from the free list
@@ -54,20 +133,44 @@ pub fn Entities(comptime components: anytype) type {
                     // Add a new entity to the end of the list
                     const top = self.entities.len;
                     self.entities.len += 1;
-                    self.entities[top] = 0;
+                    self.entities[top] = .{
+                        .generation = 0,
+                    };
                     break :index @intCast(EntityIndex, top);
                 } else {
                     return null;
                 }
             };
+
+            // XXX: don't ignore errors here...just trying things out
+            // XXX: allocate pages up front in pool when possible?
+            // Find or allocate a page for this entity
+            var entry = self.pageLists.getOrPut(
+                std.heap.page_allocator,
+                @This().archetype(entity),
+            ) catch unreachable;
+            if (!entry.found_existing) {
+                entry.value_ptr.* = std.heap.page_allocator.create(Page(@TypeOf(entity))) catch unreachable;
+            }
+            const page: *Page(@TypeOf(entity)) = @ptrCast(*Page(@TypeOf(entity)), @alignCast(@alignOf(*Page(@TypeOf(entity))), entry.value_ptr.*));
+
+            // XXX: assumes there's room in this page for now, never creates a new one
+            // Populate the entity
+            const indexInPage = page.createEntity() orelse unreachable;
+            // XXX: rename to entity
+            page.entities[indexInPage] = entity;
+            // XXX: now we wanna also store the indexinpage, as well as the page, on the entityslot
+            // so that we can look it back up later in a getComponents call or such!
+
+            // Return a handle to the entity
             return EntityHandle{
                 .index = index,
-                .generation = self.entities[index],
+                .generation = self.entities[index].generation,
             };
         }
 
-        pub fn createEntity(self: *@This()) EntityHandle {
-            return self.createEntityChecked() orelse
+        pub fn createEntity(self: *@This(), entity: anytype) EntityHandle {
+            return self.createEntityChecked(entity) orelse
                 std.debug.panic("out of entities", .{});
         }
 
@@ -77,7 +180,7 @@ pub fn Entities(comptime components: anytype) type {
             if (entity.index >= self.entities.len) {
                 return error.BadIndex;
             }
-            if (self.entities[entity.index] != entity.generation) {
+            if (self.entities[entity.index].generation != entity.generation) {
                 return error.BadGeneration;
             }
             if (self.free.len == max_entities) {
@@ -85,7 +188,7 @@ pub fn Entities(comptime components: anytype) type {
             }
 
             // Increment this entity slot's generation so future uses will fail
-            self.entities[entity.index] +%= 1;
+            self.entities[entity.index].generation +%= 1;
 
             // Add the entity to the free list
             const top = self.free.len;
@@ -97,25 +200,40 @@ pub fn Entities(comptime components: anytype) type {
             self.removeEntityChecked(entity) catch unreachable;
         }
 
+        // XXX: wait type ids are a thing? do we even need this?
         fn componentMask(comptime component: []const u8) Archetype {
-            comptime var i = 0;
-            inline for (@typeInfo(@TypeOf(components)).Struct.fields) |c| {
-                if (std.mem.eql(u8, c.name, component)) {
-                    comptime var mask = Archetype.initEmpty();
-                    mask.set(i);
-                    return mask;
+            comptime {
+                inline for (@typeInfo(RegisteredComponents).Struct.fields, 0..) |c, i| {
+                    if (std.mem.eql(u8, c.name, component)) {
+                        var mask = Archetype.initEmpty();
+                        mask.set(i);
+                        return mask;
+                    }
                 }
-                i += 1;
+                @compileError("component '" ++ component ++ "' not registered");
             }
-            @compileError("component " ++ component ++ " not registered");
         }
 
-        fn archetype(comptime archetypeComponents: anytype) Archetype {
-            comptime var result = Archetype.initEmpty();
-            inline for (archetypeComponents) |component| {
-                result = result.unionWith(componentMask(component));
+        fn componentType(comptime component: []const u8) type {
+            comptime {
+                // XXX: use std.meta.fields instead?
+                inline for (@typeInfo(RegisteredComponents).Struct.fields) |c| {
+                    if (std.mem.eql(u8, c.name, component)) {
+                        return c.type;
+                    }
+                }
+                @compileError("component '" ++ component ++ "' not registered");
             }
-            return result;
+        }
+
+        fn archetype(components: anytype) Archetype {
+            comptime {
+                var result = Archetype.initEmpty();
+                inline for (@typeInfo(@TypeOf(components)).Struct.fields) |component| {
+                    result = result.unionWith(componentMask(component.name));
+                }
+                return result;
+            }
         }
     };
 }
@@ -124,7 +242,7 @@ test "limits" {
     // The max entity id should be considered invalid
     std.debug.assert(max_entities < std.math.maxInt(EntityIndex));
 
-    var entities = try Entities(.{}).init();
+    var entities = try Entities(struct {}).init();
     defer entities.deinit();
     var created = std.ArrayList(EntityHandle).init(std.testing.allocator);
     defer created.deinit();
@@ -133,11 +251,11 @@ test "limits" {
     {
         var i: EntityIndex = 0;
         while (i < max_entities) : (i += 1) {
-            const entity = entities.createEntity();
+            const entity = entities.createEntity(.{});
             try std.testing.expectEqual(EntityHandle{ .index = i, .generation = 0 }, entity);
             try created.append(entity);
         }
-        try std.testing.expect(entities.createEntityChecked() == null);
+        try std.testing.expect(entities.createEntityChecked(.{}) == null);
     }
 
     // Remove all the entities
@@ -155,39 +273,39 @@ test "limits" {
         while (i < max_entities) : (i += 1) {
             try std.testing.expectEqual(
                 EntityHandle{ .index = i, .generation = 1 },
-                entities.createEntity(),
+                entities.createEntity(.{}),
             );
         }
-        try std.testing.expect(entities.createEntityChecked() == null);
+        try std.testing.expect(entities.createEntityChecked(.{}) == null);
     }
 
     // Wrap a generation counter
     {
         var entity = EntityHandle{ .index = 0, .generation = std.math.maxInt(EntityGeneration) };
-        entities.entities[entity.index] = entity.generation;
+        entities.entities[entity.index].generation = entity.generation;
         entities.removeEntity(entity);
         try std.testing.expectEqual(
             EntityHandle{ .index = 0, .generation = @intCast(EntityGeneration, 0) },
-            entities.createEntity(),
+            entities.createEntity(.{}),
         );
     }
 }
 
 test "create destroy" {
-    var entities = try Entities(.{}).init();
+    var entities = try Entities(struct {}).init();
     defer entities.deinit();
 
-    const entity_0_0 = entities.createEntity();
-    const entity_1_0 = entities.createEntity();
-    const entity_2_0 = entities.createEntity();
-    const entity_3_0 = entities.createEntity();
+    const entity_0_0 = entities.createEntity(.{});
+    const entity_1_0 = entities.createEntity(.{});
+    const entity_2_0 = entities.createEntity(.{});
+    const entity_3_0 = entities.createEntity(.{});
 
     entities.removeEntity(entity_1_0);
     entities.removeEntity(entity_3_0);
 
-    const entity_3_1 = entities.createEntity();
-    const entity_1_1 = entities.createEntity();
-    const entity_4_0 = entities.createEntity();
+    const entity_3_1 = entities.createEntity(.{});
+    const entity_1_1 = entities.createEntity(.{});
+    const entity_4_0 = entities.createEntity(.{});
 
     try std.testing.expectEqual(EntityHandle{ .index = 0, .generation = 0 }, entity_0_0);
     try std.testing.expectEqual(EntityHandle{ .index = 1, .generation = 0 }, entity_1_0);
@@ -199,10 +317,10 @@ test "create destroy" {
 }
 
 test "safety checks" {
-    var entities = try Entities(.{}).init();
+    var entities = try Entities(struct {}).init();
     defer entities.deinit();
 
-    const entity = entities.createEntity();
+    const entity = entities.createEntity(.{});
     entities.removeEntity(entity);
     try std.testing.expectError(error.BadGeneration, entities.removeEntityChecked(entity));
     try std.testing.expectError(error.BadIndex, entities.removeEntityChecked(EntityHandle{
@@ -212,17 +330,29 @@ test "safety checks" {
 }
 
 test "archetype masks" {
-    var entities = try Entities(.{ .x = u32, .y = u8 }).init();
+    var entities = try Entities(struct { x: u8, y: u8 }).init();
     defer entities.deinit();
 
-    var archetype = @TypeOf(entities).Archetype.initEmpty();
-    try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{}));
-    try std.testing.expect(!archetype.eql(comptime @TypeOf(entities).archetype(.{"y"})));
-    archetype.set(0);
-    try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{"x"}));
-    archetype.set(1);
-    try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{ "x", "y" }));
-    try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{ "y", "x" }));
-    archetype.unset(0);
-    try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{"y"}));
+    // XXX: ...
+    // var archetype = @TypeOf(entities).Archetype.initEmpty();
+    // try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{}));
+    // try std.testing.expect(!archetype.eql(comptime @TypeOf(entities).archetype(.{ .y = u8 })));
+    // archetype.set(0);
+    // try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{ .x = u32 }));
+    // archetype.set(1);
+    // try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{ .x = u32, .y = u8 }));
+    // try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{ .y = u8, .x = u32 }));
+    // archetype.unset(0);
+    // try std.testing.expectEqual(archetype, comptime @TypeOf(entities).archetype(.{ .y = u8 }));
+
+    // const A = struct {
+    //     x: u8,
+    //     y: u8,
+    // };
+    // const B = struct {
+    //     y: u8,
+    //     x: u8,
+    // };
+    _ = entities.createEntity(.{ .x = 1, .y = 2 });
+    _ = entities.createEntity(.{ .y = 1, .x = 2 });
 }
