19005c8d4913abba292fed35996ab36aa006adaf
Mason Remaley <mason@anthropicstudios.com>
2025-05-16 20:10:45 -0700

    Adds docs to upload queue, fixes submit to reset writer

--- src/ImageUploadQueue.zig
+++ src/ImageUploadQueue.zig
@@ -1,3 +1,42 @@
+//! A color image upload queue.
+//!
+//! Operates on a fixed size staging buffer, writes to whatever memory the caller provides.
+//!
+//! To use, first call `beginWrite`, and then write your data to the provided writer. When all of
+//! your uploads have been queued, call `submit`. This must be done within a frame.
+//!
+//! # Uploading Synchronously
+//!
+//! If you only need to upload a small amount of data, you can probably load your images
+//! synchronously to a single `ImageUploadQueue`.
+//!
+//! Attempting to calculate the exact size of the staging buffer or image memory isn't recommended
+//! as it limits your flexibility. When working with a small amount of data, this shouldn't be a
+//! concern, and when working with a large amount of data you typically upload asynchronously which
+//! means you're reusing staging buffers anyway.
+//!
+//! # Uploading Asynchronously
+//!
+//! If you're uploading a large amount of data, you probably should do it asynchronously.
+//!
+//! Eventually `gpu` will get support for multiple queues
+//! (https://github.com/Games-by-Mason/gpu/issues/1), but even still you likely want to
+//! support hardware that only provides a single queue. This means that you need to divide up your
+//! work into frame sized chunks.
+//!
+//! The recommended approach is to create one `ImageUploadQueue` per frame in flight. Assets should
+//! be loaded from disk on a background thread or threads. The main thread polls to see if the
+//! background threads have completed any work, and if they have, checks if there's space in the
+//! current upload queue. If there is, it writes them.
+//!
+//! At the end of each frame, the current upload queue is submitted.
+//!
+//! By tuning the size of the staging buffer, you can limit how much work is done one any single
+//! frame and provide a smooth experience. Keep in mind that the bottleneck is likely the read from
+//! disk, not the GPU upload, and you can set your values accordingly. Setting the buffer too small
+//! is unlikely to affect performance much, but it needs to be at least as large as the largest
+//! image.
+
 const gpu = @import("gpu");
 
 const Gx = gpu.Gx;
@@ -89,5 +128,6 @@ pub fn submit(self: *@This(), gx: *Gx) void {
     if (self.cb.unwrap()) |cb| {
         cb.submit(gx);
         self.cb = .none;
+        self.writer.pos = 0;
     }
 }
