e9a67b4837bcbc7c8dbd6aa7a0203086f6d2b2c0
Mason Remaley <mason@anthropicstudios.com>
2025-06-28 20:17:06 -0700

    Fixes composite uniform type, updates sdl_zig

--- src/shaders/descs/post.glsl
+++ src/shaders/descs/post.glsl
@@ -2,6 +2,6 @@
 
 #ifdef GL_COMPUTE_SHADER
 	layout(scalar, binding = 0) readonly buffer SceneUbo { Scene scene; };
-	layout(binding = 1, rgba8) readonly uniform image2D color_buffer;
-	layout(binding = 2, rgba8) readonly uniform image2D composite;
+	layout(binding = 1, rgba8) readonly uniform image2D color_buffer_r;
+	layout(binding = 2, rgba8) uniform image2D composite_rw;
 #endif
